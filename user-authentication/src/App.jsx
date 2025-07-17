27fdeba0203048cb031417162dfb4c7c0aefc9a5
iliyas <iliyassipai861@gmail.com>
2025-07-17 09:48:56 +0530

    use Navigation

--- user-authentication/src/App.jsx
+++ user-authentication/src/App.jsx
@@ -1,14 +1,25 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import { BrowserRouter } from 'react-router-dom'
 import './App.css'
 import LoginCompo from './pages/LoginCompo'
+import ProfileCompo from './pages/ProfileCompo'
+import { BrowserRouter,Router,Routes,Route } from 'react-router-dom'
+import NavbarCompo from './NavbarCompo'
 
 function App() {
   return (
     <>
-    <LoginCompo/>
+       {/* <Router> */}
+       <NavbarCompo /> 
+      <Routes>
+        <Route path='/' element={<LoginCompo/>}/>
+        <Route path='/login' element={<LoginCompo/>}/>
+        <Route path='/profile' element={<ProfileCompo/>}/>
+      </Routes>
+      {/* </Router> */}
+    {/* <LoginCompo/>
+    <ProfileCompo/> */}
     </>
   )
 }
--- user-authentication/src/pages/LoginCompo.jsx
+++ user-authentication/src/pages/LoginCompo.jsx
@@ -1,44 +1,70 @@
 import React from "react";
 import { useState } from "react";
 import axios from "axios";
+import { Navigate, useNavigate } from "react-router";
 const LoginCompo = () => {
-
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
-    const handleChange = () =>{
-       const payload = {
-        email: email,
-        password:password
-       }
-       console.log(password,email);
-       axios.post(" https://api.escuelajs.co/api/v1/auth/login",payload)
-       .then((res)=>{
-        localStorage.setItem("token", JSON.stringify( res.data.access_token))
-        alert("login is compplted")
-         console.log("login is compplted",res);
-        })
-        .catch((err)=>{
-          console.log("login is fail");
-        })
-      }
+  const navigate = useNavigate();
+  const handleChange = () => {
+    const payload = {
+      email: email,
+      password: password,
+    };
+    console.log(password, email);
+    axios
+      .post(" https://api.escuelajs.co/api/v1/auth/login", payload)
+      .then((res) => {
+        localStorage.setItem("token", JSON.stringify(res.data.access_token));
+        alert("login is compplted");
+        console.log("login is compplted", res);
+        navigate("/profile");
+      })
+      .catch((err) => {
+        console.log("login is fail");
+      });
+  };
   return (
     <>
-      <div className="container">
-        <h1 className="text-center mb-80">User Authentication</h1>
-        <div className="row justify-content-center mt-5 ">
-          <div className="col-md-6 w-500">
-            <form action="#" className="p-4 border rounded shadow  p-4">
-              <label htmlFor="email">Email:</label>
-              <input onChange={(e)=>setEmail(e.target.value)} type="email" id="email" className="form-control" />
-              <label htmlFor="password">Password:</label>
-              <input onChange={(e)=>setPassword(e.target.value)} type="password" id="password" className="form-control" />
-              <button onClick={handleChange} type="submit" className="btn btn-primary">
-                Login
-              </button>
-            </form>
+      <div className="card p-4 shadow w-100 w-md-50 mx-auto">
+        <h4 className="text-center text-secondary mb-4">
+          🔐 User Authentication
+        </h4>
+        <form onSubmit={(e) => e.preventDefault()}>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">
+              Email address
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="form-control"
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Enter your email"
+              required
+            />
           </div>
-        </div>
+
+          <div className="mb-3">
+            <label htmlFor="password" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="form-control"
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Enter your password"
+              required
+            />
+          </div>
+
+          <div className="d-grid">
+            <button onClick={handleChange} className="btn btn-primary">
+              Login
+            </button>
+          </div>
+        </form>
       </div>
     </>
   );
