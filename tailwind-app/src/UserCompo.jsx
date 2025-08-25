94f0e4da680ca16e7669fb59b5d03b26c6a072e0
iliyas <iliyassipai861@gmail.com>
2025-08-25 15:27:54 +0530

    Initial commit with all React projects

--- tailwind-app/src/UserCompo.jsx
+++ tailwind-app/src/UserCompo.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
 const UserCompo = () => {
+  const data = []
   return (
     <div>
       <h1>This for user</h1>
+      <div>
+           <h1>Hello</h1>
+           <button>save data</button>
+      </div>
     </div>
   )
 }
