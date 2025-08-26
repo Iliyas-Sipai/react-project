5e73731c7adab3af88812c88ac771791e40d7ef6
iliyas <iliyassipai861@gmail.com>
2025-08-26 12:00:06 +0530

    employe task list are ready

--- emp-manage/src/components/Tasklist/TaskList.jsx
+++ emp-manage/src/components/Tasklist/TaskList.jsx
@@ -3,7 +3,25 @@ import React from "react";
 const TaskList = () => {
   return (
     <div id="task-list" className="flex item-center justify-startr gap-5 flex-nowrap overflow-x-auto h-[54%] py-5 w-full mt-10">
-      <div className="flex-shrink-0 h-full bg-red-400 w-[300px] p-5 rounded-xl">
+      <div className="flex-shrink-0 h-full bg-yellow-400 w-[300px] p-5 rounded-xl">
+        <div className="flex justify-between items-center text-white">
+           <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
+           <h4 className="text-sm">20 feb 2025</h4>
+        </div>
+        <h2 className="mt-5 text-2xl text-white font-semibold">Make Rreact project</h2>
+         <p className="text-xl mt-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+           perspiciatis maiores sint tenetur hic eos.</p> 
+      </div>
+      <div className="flex-shrink-0 h-full bg-green-400 w-[300px] p-5 rounded-xl">
+        <div className="flex justify-between items-center text-white">
+           <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
+           <h4 className="text-sm">20 feb 2025</h4>
+        </div>
+        <h2 className="mt-5 text-2xl text-white font-semibold">Make Rreact project</h2>
+         <p className="text-xl mt-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+           perspiciatis maiores sint tenetur hic eos.</p> 
+      </div>
+      <div className="flex-shrink-0 h-full bg-blue-400 w-[300px] p-5 rounded-xl">
         <div className="flex justify-between items-center text-white">
            <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
            <h4 className="text-sm">20 feb 2025</h4>
