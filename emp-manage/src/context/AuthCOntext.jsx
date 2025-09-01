fee43e4b12bd6a881d6718c5f26c8c4d928174cc
iliyas <iliyassipai861@gmail.com>
2025-09-01 22:39:43 +0530

    save change

--- emp-manage/src/context/AuthCOntext.jsx
+++ emp-manage/src/context/AuthCOntext.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const AuthCOntext = ({childern}) => {
   return (
     <div>
-        {/* <h1 className='text-white'>Hello</h1> */}
+        <h1 className='text-white'>Hello</h1>
         <h1>{childern}</h1>
     </div>
   )
